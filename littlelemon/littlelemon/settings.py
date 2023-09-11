229cecbc2465ee6338d045bdaa0425243816b87b
qiaod <qiaod@aquait.co.jp>
2023-09-11 17:24:30 +0900

    User Authentication

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'restaurant',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSS': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
+
+#add the following line
+DJOSER={"USER_ID_FIELD":"username"}
